e03a9cb30ed11ca0a852aa483e8bda3fc9244618
jinow9595 <jinow9595@gmail.com>
2022-03-26 13:04:42 +0900

    fetch coin api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coin, setCoin] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(response => response.json())
+      .then(json => {
+        setCoin(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div className="App">
+    <div>
+      <h1>Coin Tracker {loading ? "" : coin.length}</h1>
+      {loading ? <strong>Loading...</strong> : (
+        <select>
+          {coin.map((coin) => <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
+        </select>
+      )}
     </div>
   );
 }
